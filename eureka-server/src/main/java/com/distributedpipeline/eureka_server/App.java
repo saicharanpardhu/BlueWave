bc867c9a81f1b1a49a7ec2f37af5a702d4e75f80
abinav.karna <abinav.karna@cgi.com>
2017-10-29 23:48:04 +0530

    Update App.java

--- eureka-server/src/main/java/com/distributedpipeline/eureka_server/App.java
+++ eureka-server/src/main/java/com/distributedpipeline/eureka_server/App.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//Main class
 public class App 
 {
     public static void main( String[] args )
