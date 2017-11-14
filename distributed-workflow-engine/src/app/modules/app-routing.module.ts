931b956154a294cfdcc9b148ef0b090998e26a85
abhinav <abhinavkarn55@gmail.com>
2017-11-14 10:06:39 +0530

    update

--- distributed-workflow-engine/src/app/modules/app-routing.module.ts
+++ distributed-workflow-engine/src/app/modules/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   {path : 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
   {path : 'reports', component: ReportsComponent, canActivate: [AuthGuardService]},
   {path : 'addworkflow', component: CreateWorkflowComponent, canActivate: [AuthGuardService]},
-  {path : 'executeworkflow', component: ExecuteWorkflowComponent, canActivate: [AuthGuardService]}  
+  {path : 'executeworkflow', component: ExecuteWorkflowComponent, canActivate: [AuthGuardService]} , 
   {path : 'workflow/workflowdetails', component: WorkflowDetailsComponent ,canActivate: [AuthGuardService] } 
   
 ];
