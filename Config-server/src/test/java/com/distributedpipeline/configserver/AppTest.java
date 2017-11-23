9a6b1186bd29ef67c782bdcb5fb768797ca3108e
prashant.maroti <prashant.maroti@cgi.com>
2017-11-23 15:44:25 +0530

    Update AppTest.java

--- Config-server/src/test/java/com/distributedpipeline/configserver/AppTest.java
+++ Config-server/src/test/java/com/distributedpipeline/configserver/AppTest.java
@@ -1,4 +1,4 @@
-package com.distributedpipeline.Config_server;
+package com.distributedpipeline.configserver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
