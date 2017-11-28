d827c64fd7234fe401e32a1a6b944545f5c94af7
specter221 <vaibhavakshay19@gmail.com>
2017-11-28 17:23:45 +0530

    angular docker added and api gateway added

--- user-persistence/src/main/java/com/distributedpipeline/userpersistence/DemoApplication.java
+++ user-persistence/src/main/java/com/distributedpipeline/userpersistence/DemoApplication.java
@@ -3,9 +3,11 @@ package com.distributedpipeline.userpersistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class DemoApplication {
 
 	public static void main(String[] args) {
