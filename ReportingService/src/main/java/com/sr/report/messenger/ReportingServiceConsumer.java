dabd344f035467813b241982717b371cdd0d820e
vavila.pardhu <vavila.pardhu@cgi.com>
2017-11-29 16:38:34 +0530

    Update  saveing condition change inReportingServiceConsumer.java

--- ReportingService/src/main/java/com/sr/report/messenger/ReportingServiceConsumer.java
+++ ReportingService/src/main/java/com/sr/report/messenger/ReportingServiceConsumer.java
@@ -2,6 +2,7 @@ package com.sr.report.messenger;
 
 import java.util.concurrent.CountDownLatch;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,18 +26,62 @@ public class ReportingServiceConsumer {
 
 	@Autowired
 	private ReportModelCRUDRepository reportModelCRUDRepository;
-	
-    @KafkaListener(topics="${kafka.topic.projectModeltopic}",
+    
+    @KafkaListener(topics="reports",
     		containerFactory = "projectModelKafkaListenerContainerFactory")
    public void processMessage(ReportModel report) {
     	
-        
+    	System.out.println("Report received from kafka --------------------------------------------------");
+    	System.out.println("job id "+report.getJobId());
+    	System.out.println("task alias "+report.getTaskAlias());
+    	System.out.println("task start time "+report.getTaskStartTime());
+    	System.out.println("task end time "+report.getTaskEndTime());
+    	
         latch.countDown();
-        reportModelCRUDRepository.save(report);
-    	System.out.println(report.toString());
+        if(report.getJobId()!=null&&report.getTaskAlias()!=null) {
+        ReportModel report_db = reportModelCRUDRepository.findByJobIdAndTaskAlias(report.getJobId(),report.getTaskAlias());
+        if(report_db!=null) {
+        	if(report_db.getJobEndTime() == null)
+        	{
+        		report_db.setJobEndTime(report.getJobEndTime());
+        	}
+        	if(report_db.getJobStartTime() == null)
+        	{
+        		report_db.setJobStartTime(report.getJobStartTime());
+        	}
+        	if(report_db.getJobStatus() == null)
+        	{
+        		report_db.setJobStatus(report.getJobStatus());
+        	}
+        	if(report_db.getTaskEndTime() == null)
+        	{
+        		report_db.setTaskEndTime(report.getTaskEndTime());
+        	}
+        	if(report_db.getTaskStartTime() == null)
+        	{
+        		report_db.setTaskStartTime(report.getTaskStartTime());
+        	}
+        	if(report_db.getTaskLogs() == null)
+        	{
+        		report_db.setTaskLogs(report.getTaskLogs());
+        	}
+        	if(report_db.getWorkFlowName() == null)
+        	{
+        		report_db.setWorkFlowName(report.getWorkFlowName());
+        	}
+        	
+        	reportModelCRUDRepository.save(report_db);
+        }
+        else {
+        	reportModelCRUDRepository.save(report);
+        }
+        
+        
+        
+        
         
         
    }
     
    
-}
+    }}
