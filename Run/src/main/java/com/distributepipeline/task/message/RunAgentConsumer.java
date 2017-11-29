e1a944b3a6bc2025344ee64afef5e91d3a7af404
abinav.karna <abinav.karna@cgi.com>
2017-11-29 10:52:20 +0530

    Update RunAgentConsumer.java

--- Run/src/main/java/com/distributepipeline/task/message/RunAgentConsumer.java
+++ Run/src/main/java/com/distributepipeline/task/message/RunAgentConsumer.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.sql.Timestamp;
+import java.util.Date;
 import java.util.concurrent.CountDownLatch;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +26,7 @@ public class RunAgentConsumer {
 	    return latch;
 	  }
 	
-//	 @Autowired 
-//	 private Input input;
+
 	  @Autowired
 	  ReportModel reportModel;
 	 
@@ -45,13 +44,7 @@ public class RunAgentConsumer {
 	
 	 private String jobid;
 	 private String taskname;
-//	public Input getInput() {
-//		return input;
-//	}
-//
-//	public void setInput(Input input) {
-//		this.input = input;
-//	}
+
 
 	public String getJobid() {
 		return jobid;
@@ -80,34 +73,19 @@ public class RunAgentConsumer {
 				String fname="";					//"/GoPlacesBackend-0.1.0.jar";
 				String fnameJar = "";
 				
-				String pathProject = "/home/imbatch1/Task_source";// /Spring_restapi/target";
+				String pathProject = "/home/jaydeep/Desktop/Task_source/"+inputdata.getJobId();// /Spring_restapi/target";
 				
 				System.out.println("p "+pathProject);
-				
-//				final File folder = new File(path);
-//				listFilesForFolder(folder);
-				
-//				File dir = new File(pathProject);
-//				System.out.println(dir.isDirectory());
-//				File[] fListProject = dir.listFiles();
-//				
-//				System.out.println(fListProject);
-//																	//	getting the project name like Spring_restapi
-//				
-//				for(File flist : fListProject) {
-//				
-//					fname = flist.getName();
-//					System.out.println(fname);
-//			}
+
 
 				
 				String fname2[] = inputdata.getInput()[0].split("/");
 			     String fileName = fname2[fname2.length-1].split("\\.")[0];
 			     System.out.println(fileName);											//getting the fileName of .jar
 
-				String pathJar = "/home/imbatch1/Task_source/"+fileName+"/target";
+				String pathJar = "/home/jaydeep/Desktop/Task_source/"+inputdata.getJobId()+"/"+fileName+"/target";
 				System.out.println(pathJar);
-				reportModel.setTaskStartTime(new Timestamp(System.currentTimeMillis()));
+				reportModel.setTaskStartTime(new Date(System.currentTimeMillis()));
 				reportModel.setTaskAlias(inputdata.getTaskname());
 				reportModel.setJobId(inputdata.getJobId());
 				System.out.println("start time:"+ reportModel.getTaskStartTime());
@@ -131,43 +109,16 @@ public class RunAgentConsumer {
 				
 				System.out.println("FileFound "+fileFound);
 				
-//				if(fileFound == 1) {
-//					path2 += fname2;
-//				}
+
 
 				File directory = new File(pathJar+fnameJar);
 
 				System.out.println("Task Running...");
 				
 				System.out.println("java -jar "+ pathJar+"/"+fnameJar);
-//				Process1 process= Runtime.getRuntime().exec(command)
 				Process process = Runtime.getRuntime().exec("java -jar "+ pathJar+"/"+fnameJar);
 				System.out.println(pathJar +" "+ fnameJar);
-				
-//			
-                
-				
-				
-				
-				
-//				BufferedInputStream catOutput= new BufferedInputStream(process.getInputStream());
-//				int read = 0;
-//				byte[] output2 = new byte[1024];
-//				while ((read = catOutput.read(output2)) != -1) {
-//					System.out.println(output2[read]);
-//				}
-//				
-//				ProcessBuilder process2 = new ProcessBuilder(pathJar, "-jar", fnameJar);
-//				process2.directory(new File("preferred/working/directory"));
-//				Process process3 = process2.start();
-				
-				//process.waitFor();
-				
-				//System.out.println("Exit "+ process.exitValue());
-				
-				
-				
-//		          writer.close();
+
 
 			if(process.isAlive() ) {
 					String []str= {"Run Task Complete","https://172.23.238.186:8321/gitsearch/repo"};
@@ -176,19 +127,12 @@ public class RunAgentConsumer {
 					output.setStderr("task complete");
 					System.out.println("task complete!");
 					
-					reportModel.setTaskEndTime(new Timestamp(System.currentTimeMillis()));
+					
 					reportModel.setTaskAlias(inputdata.getTaskname());
 					reportModel.setJobId(inputdata.getJobId());
 					reportModel.setTaskLogs("[INFO]:"+output.getStderr());
 					System.out.println(reportModel.getTaskEndTime());
-					producer.sendReport(reportModel);     
-				
-					output.setUserName(inputdata.getUserName());
-					output.setJobId(inputdata.getJobId());
-					output.setTaskName(inputdata.getTaskname());
-					producer.sendMessage();
-			
-		
+					
 					StringBuffer output1 = new StringBuffer();
 			          BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			          String line = "";                       
@@ -196,16 +140,27 @@ public class RunAgentConsumer {
 			                  output1.append(line + "\n");
 			                 
 			          }
-			          PrintWriter writer = new PrintWriter("/home/imbatch1/distributedpipeline/Run/output1.txt", "UTF-8");
+			          PrintWriter writer = new PrintWriter("/home/jaydeep/Desktop/output1.txt", "UTF-8");
 			          consoleOutput.setTaskName(inputdata.getTaskname());
 			          consoleOutput.setUserName(inputdata.getUserName());
 			          
 			          consoleOutput.setConsole(output1.toString());
 			          producer.sendMessageConsole(consoleOutput);    
 			          writer.println(output1);
-			          System.out.println("AAAAAAAAAAAAA");
 					
 			          System.out.println(process.getOutputStream());
+			          reportModel.setTaskEndTime(new Date(System.currentTimeMillis()));  
+					producer.sendReport(reportModel);  
+					System.out.println("start time:"+ reportModel.getTaskStartTime());
+					System.out.println("end Time:"+ reportModel.getTaskEndTime());
+				
+					output.setUserName(inputdata.getUserName());
+					output.setJobId(inputdata.getJobId());
+					output.setTaskName(inputdata.getTaskname());
+					producer.sendMessage();
+			
+		
+				
 					
 			}
 	
