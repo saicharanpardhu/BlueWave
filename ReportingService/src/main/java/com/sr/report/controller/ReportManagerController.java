07f465579305cbcaa5d87587687b90f9284a0d77
akshay.vaibhav <akshay.vaibhav@cgi.com>
2017-11-30 21:06:54 +0530

    Update ReportManagerController.java

--- ReportingService/src/main/java/com/sr/report/controller/ReportManagerController.java
+++ ReportingService/src/main/java/com/sr/report/controller/ReportManagerController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import com.sr.report.model.ReportModel;
 import com.sr.report.repository.*;
 
