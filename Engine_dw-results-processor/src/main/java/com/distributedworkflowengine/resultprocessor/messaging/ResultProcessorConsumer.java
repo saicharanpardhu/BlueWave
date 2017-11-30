0351bb2a2a98777e55ff9409ceb66f1fd7a0e481
jaydeep.jain <jaydeep.jain@cgi.com>
2017-11-30 09:53:16 +0530

    Update ResultProcessorConsumer.java

--- Engine_dw-results-processor/src/main/java/com/distributedworkflowengine/resultprocessor/messaging/ResultProcessorConsumer.java
+++ Engine_dw-results-processor/src/main/java/com/distributedworkflowengine/resultprocessor/messaging/ResultProcessorConsumer.java
@@ -74,7 +74,7 @@ public class ResultProcessorConsumer {
 		logger.info(model);
 		if(model.getOutput()!=null)
 		user.setOutput(model.getOutput()[0]);
-		user.setUserName(model.getJobId);
+		user.setUserName(model.getJobId());
 		logger.info(user.getUserName());
 		producer.sendMessageSocket(user);
 		if(model.getErrcode()==200)
