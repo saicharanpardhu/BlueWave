22c3188eea4ba418d96a21fd45549a7f7f7f13e0
prashant.maroti <prashant.maroti@cgi.com>
2017-12-05 21:20:11 +0530

    discovery client enabled

--- Test/src/main/java/com/distributedpipelilne/testAgent/App.java
+++ Test/src/main/java/com/distributedpipelilne/testAgent/App.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * this is the main driver class
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
 
