2286302d5d1392e04726a61b9e07687ccfdf299c
abhinav <abhinavkarn55@gmail.com>
2017-12-05 14:46:52 +0530

    adding test task

---
+++ Test/src/main/java/com/distributedpipelilne/testAgent/domain/ReportModel.java
@@ -0,0 +1,115 @@
+package com.distributedpipelilne.testAgent.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Table
+public class ReportModel {
+    
+  private String jobId; //job-id
+    
+    @Column
+   private String workFlowName; //name of the workflow
+    
+    @Column
+   private Date jobStartTime;
+    
+    @Column
+    private Date jobEndTime;
+    
+    @Column
+    private String jobStatus;
+    
+   private String taskAlias;
+    
+    @Column
+   private Date taskStartTime;
+    
+    @Column
+   private Date taskEndTime;
+    
+    @Column
+   private String taskLogs;
+    
+    public String getTaskAlias() {
+        return taskAlias;
+    }
+
+    public void setTaskAlias(String taskAlias) {
+        this.taskAlias = taskAlias;
+    }
+
+    public Date getTaskStartTime() {
+        return taskStartTime;
+    }
+
+    public void setTaskStartTime(Date taskStartTime) {
+        this.taskStartTime = taskStartTime;
+    }
+
+    public Date getTaskEndTime() {
+        return taskEndTime;
+    }
+
+    public void setTaskEndTime(Date taskEndTime) {
+        this.taskEndTime = taskEndTime;
+    }
+
+    public String getTaskLogs() {
+        return taskLogs;
+    }
+
+    public void setTaskLogs(String taskLogs) {
+        this.taskLogs = taskLogs;
+    }
+
+
+
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getWorkFlowName() {
+        return workFlowName;
+    }
+
+    public void setWorkFlowName(String workFlowName) {
+        this.workFlowName = workFlowName;
+    }
+
+    public Date getJobStartTime() {
+        return jobStartTime;
+    }
+
+    public void setJobStartTime(Date jobStartTime) {
+        this.jobStartTime = jobStartTime;
+    }
+
+    public Date getJobEndTime() {
+        return jobEndTime;
+    }
+
+    public void setJobEndTime(Date jobEndTime) {
+        this.jobEndTime = jobEndTime;
+    }
+
+    public String getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(String jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+    
+    
+}
---
+++ Test/src/main/java/com/distributedpipelilne/testAgent/message/testAgentProducerConfig.java
@@ -0,0 +1,108 @@
+package com.distributedpipelilne.testAgent.message;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.distributedpipelilne.testAgent.domain.ConsoleOutput;
+import com.distributedpipelilne.testAgent.domain.Output;
+import com.distributedpipelilne.testAgent.domain.ReportModel;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+@Configuration
+@EnableKafka
+public class testAgentProducerConfig {
+    @Bean
+    public ProducerFactory<String, Output> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+          "172.23.238.178:9092");
+        configProps.put(
+          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 
+          StringSerializer.class);
+        configProps.put(
+          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
+          JsonSerializer.class);
+        
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+ 
+    @Bean
+    public KafkaTemplate<String, Output> kafkaReportTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+  
+    
+    @Bean
+    public ProducerFactory<String, String> producerStringFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+          "172.23.238.178:9092");
+        configProps.put(
+          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 
+          StringSerializer.class);
+        configProps.put(
+          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
+          StringSerializer.class);
+        
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+ 
+    @Bean
+    public KafkaTemplate<String, String> kafkaStringTemplate() {
+        return new KafkaTemplate<>(producerStringFactory());
+    }
+    
+    @Bean
+    public ProducerFactory<String, ConsoleOutput> producerConsoleFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+          "172.23.238.178:9092");
+        configProps.put(
+          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 
+          StringSerializer.class);
+        configProps.put(
+          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
+          JsonSerializer.class);
+        
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+ 
+    @Bean
+    public KafkaTemplate<String, ConsoleOutput> kafkaConsoleTemplate() {
+        return new KafkaTemplate<>(producerConsoleFactory());
+    }
+    
+    @Bean
+    public ProducerFactory<String, ReportModel> producerReportingFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+          "172.23.238.178:9092");
+        configProps.put(
+          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+          StringSerializer.class);
+        configProps.put(
+          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+          JsonSerializer.class);
+        
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, ReportModel> kafkaReportingTemplate() {
+        return new KafkaTemplate<>(producerReportingFactory());
+    }
+}
